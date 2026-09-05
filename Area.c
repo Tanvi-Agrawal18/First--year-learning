bedf00e61ebcaac661e6e4048316fa353443360f
Tanvi-Agrawal18 <tanvi510agrawal@gmail.com>
2026-09-05 22:00:04 +0530

    Update Area.c

--- Area.c
+++ Area.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  float celsius,farenheit;
+  printf("Enter temperature in celsius");
+  scanf("%f", &celsius);
+farebheit= (celsius × 9/5)+32
+  printf("temperature in Fahrenheit in %.2f," farenheit);
 
+return 0;
+}
+  
